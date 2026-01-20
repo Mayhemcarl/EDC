2b6d31742a28720957adbcfb55437c8b3de5fe7c
Mayhemcarl <mayhemcarl@gmail.com>
2026-01-20 12:47:09 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,13 +2,16 @@ importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.12.4/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCdp2xekXWXGorVDXtGwzC73N-F4_Ig4gU",
-  authDomain: "elemental-dojo-curico.firebaseapp.com",
-  projectId: "elemental-dojo-curico",
-  storageBucket: "elemental-dojo-curico.firebasestorage.app",
-  messagingSenderId: "39532293146",
-  appId: "1:39532293146:web:0c44ace849aeed5f5335a3",
-  measurementId: "G-YMBD4QWWMY"
+  "projectId": "elemental-dojo-curico",
+  "appId": "1:39532293146:web:0c44ace849aeed5f5335a3",
+  "databaseURL": "https://elemental-dojo-curico-default-rtdb.firebaseio.com",
+  "storageBucket": "elemental-dojo-curico.firebasestorage.app",
+  "apiKey": "AIzaSyCdp2xekXWXGorVDXtGwzC73N-F4_Ig4gU",
+  "authDomain": "elemental-dojo-curico.firebaseapp.com",
+  "messagingSenderId": "39532293146",
+  "measurementId": "G-YMBD4QWWMY",
+  "projectNumber": "39532293146",
+  "version": "2"
 });
 
 const messaging = firebase.messaging();
